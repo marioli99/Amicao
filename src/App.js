abaeee46f55c2779ce75aab882c248799faee55f
marioli99 <mazmariana.mo@gmail.com>
2022-04-13 23:14:25 -0300

    subindo projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <>
+      <h2>Amicão</h2>
+      <nav>
+        <ul>
+          <li>Adote um cão</li>
+          <li>Adote um gato</li>
+          <li>Institucional</li>
+        </ul>
+      </nav>
+      </>
+    )
+  }
+}
